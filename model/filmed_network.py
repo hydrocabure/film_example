e3f4005d1510551f2aef6db78ee33aeeb35d6d2d
hydrocabure <yuitobrown@gmail.com>
2024-05-05 00:53:09 +0900

    filmed_network.py is mostly done?

--- model/filmed_network.py
+++ model/filmed_network.py
@@ -11,10 +11,12 @@ class ResnetBlock(nn.Module):
                  padding, 
                  emb_channels 
                  ):
-        
+        super().__init__() 
+
         self.in_channels = in_channels 
         self.out_channels = out_channels 
-        self.emb_channels  = emb_channels 
+
+        assert emb_channels == out_channels, "embeddingのチャンネル数とoutのチャンネル数は一致している必要があります。" 
 
         self.convactiv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
@@ -33,7 +35,7 @@ class ResnetBlock(nn.Module):
 
         y = self.convactiv(x) 
         z = self.convnorm(y) 
-        z = z * gamma + beta 
+        z = z * gamma + beta # ここがFiLM Layer相当の処理をしている
         z = self.last_activ(z) 
         z = z + y 
 
@@ -44,10 +46,39 @@ class Network(nn.Module):
     def __init__(self,
                  in_channels,
                  out_channels,
+                 layer_cfg,
+
                  ):
-        
+        super().__init__() 
+
         self.in_channels = in_channels 
         self.out_channels = out_channels 
 
-        self.layers = nn.ModuleList([]) 
+        self.preprocess = nn.Sequential(
+            nn.Conv2d(3, 64, 7, 2, 3), 
+            nn.BatchNorm2d(64), 
+            nn.ReLU(), 
+            nn.MaxPool(3, 2),
+        )
 
+        self.layers = nn.ModuleList([]) 
+        
+        for idx, (n_block, in_chan, out_chan, kernel, stride, padding, emb) in enumerate(layer_cfg):
+            for _ in range(n_block):
+                self.layers.append(ResnetBlock(in_chan, out_chan, kernel, stride, padding, emb))
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1)) 
+        self.fc = nn.Linear(out_channels, 100) 
+
+    def forward(self, x):
+        y = self.preprocess(x) 
+        
+        for layer in self.layers:
+            y = layer(y)
+
+        y = self.avgpool(y) 
+        y = y.reshape(y.shape[0], -1) 
+        y = self.fc(y) 
+
+        return y 
+    
